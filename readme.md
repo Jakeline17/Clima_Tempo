35137dba427feec8ecd3a7f6a257e8d01561b278
jakeline17 <jaquelinesouza905@gmail.com>
2025-01-06 13:31:30 -0300

    Alterações finais

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Previsão do Tempo
+Este projeto é uma aplicação simples que permite a consulta da temperatura atual do clima na cidade inserida,foi desenvolvido utilizando HTML, CSS e Typescript para o curso Trilha Front End da ADA Tech.
+
+## Demonstração
+Você pode visualizar e testar a aplicação publicada no GitHub Pages acessando o seguinte link:
+
+[link](url '')
+
+## Funcionalidades
+- Pesquisa por cidades
+- Exibição do clima atual com base na cidade inserida.
+- Integração com a API OpenWeatherAPI
+
+## Tecnologias Utilizadas
+- HTML5
+- CSS3
+- JavaScript / TypeScript
+- React.js
+
+
--- ts/index.ts
+++ ts/index.ts
@@ -16,18 +16,19 @@ form?.addEventListener("submit", async (event) => {
     return;
   }
   try{
-  const resposta = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=4ab3aeaed480b9ccb8b421e8de668c7a&lang=pt_br&units=metric`
-  );
+  const resposta = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${localizacao}&appid=4ab3aeaed480b9ccb8b421e8de668c7a&lang=pt_br&units=metric`);
   const dados = await resposta.json();
   
 
   const infos ={
     temperatura:Math.round(dados.main.temp),
     local:dados.name,
-    icone:`https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`,
+    icone:(`https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png,`)
   };
-sectionTempoInfo.innerHTML= `
+
+ 
+sectionTempoInfo.innerHTML= 
+`return
  <div class="tempo-dados">
           <h2>${infos.local}</h2>
           <span>${infos.temperatura}°C</span>
